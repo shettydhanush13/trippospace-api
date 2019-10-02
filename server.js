28726e17469647601c8937f218eba604a83615ae
dhanush shetty <dhanush@piktorlabs.com>
2019-10-02 17:11:07 +0530

    better

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-// var Trip = require('./app/models/trips')
+var Trip = require('./app/models/trips')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 var port = process.env.PORT || 3000;
 
-// mongoose.connect('mongodb://localhost:27018/codealong');
+mongoose.connect('mongodb://heroku_4bnf62cl:659mqm9veus9q1rurnobmbkq93@ds229088.mlab.com:29088/heroku_4bnf62cl');
 
 var router = express.Router();
 
@@ -24,54 +24,54 @@ router.get('/', function (req, res) {
     res.json({ message: "welcome to trippospace" });
 });
 
-// router.route('/trip')
+router.route('/trip')
 
-//     .post(function (req, res) {
-//         var trip = new Trip();
-//         trip.title = req.body.title;
-//         trip.destination = req.body.destination;
-//         trip.thumb = req.body.thumb;
-//         trip.description = req.body.description;
-//         trip.tags = req.body.tags;
-//         trip.place = req.body.place;
-//         trip.organizer = req.body.organizer;
-//         trip.booking = req.body.booking;
-//         trip.itinerary = req.body.itinerary;
-//         trip.pickup = req.body.pickup;
-//         trip.inclusions = req.body.inclusions;
-//         trip.exclutions = req.body.exclutions;
-//         trip.save(function (err) {
-//             if (err) {
-//                 res.send(err)
-//             }
-//             res.json({ message: "trip added succesfully" })
-//         });
-//     })
+    .post(function (req, res) {
+        var trip = new Trip();
+        trip.title = req.body.title;
+        trip.destination = req.body.destination;
+        trip.thumb = req.body.thumb;
+        trip.description = req.body.description;
+        trip.tags = req.body.tags;
+        trip.place = req.body.place;
+        trip.organizer = req.body.organizer;
+        trip.booking = req.body.booking;
+        trip.itinerary = req.body.itinerary;
+        trip.pickup = req.body.pickup;
+        trip.inclusions = req.body.inclusions;
+        trip.exclutions = req.body.exclutions;
+        trip.save(function (err) {
+            if (err) {
+                res.send(err)
+            }
+            res.json({ message: "trip added succesfully" })
+        });
+    })
 
-//     .get(function (req, res) {
-//         Trip.find(function (err, trip) {
-//             if (err) {
-//                 res.send(err)
-//             }
-//             res.send(trip)
-//         });
-//     });
+    .get(function (req, res) {
+        Trip.find(function (err, trip) {
+            if (err) {
+                res.send(err)
+            }
+            res.send(trip)
+        });
+    });
 
-// router.route('/trip/:tripid')
+router.route('/trip/:tripid')
 
-//     .get(function (req, res) {
+    .get(function (req, res) {
 
-//         var query = {
-//             _id: req.params.tripid
-//         };
+        var query = {
+            _id: req.params.tripid
+        };
 
-//         Trip.find(query, function (err, trip) {
-//             if (err) {
-//                 res.send(err)
-//             }
-//             res.send(trip)
-//         });
-//     });
+        Trip.find(query, function (err, trip) {
+            if (err) {
+                res.send(err)
+            }
+            res.send(trip)
+        });
+    });
 
 app.listen(port);
 
