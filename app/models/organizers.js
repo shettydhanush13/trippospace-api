2607a0a71c9ca9edb87eae06e0e471ca5cfd49ee
dhanush shetty <dhanush@piktorlabs.com>
2019-10-17 15:08:25 +0530

    added 7 methods

--- app/models/organizers.js
+++ app/models/organizers.js
@@ -4,8 +4,8 @@ var schema = mongoose.Schema
 var organizersSchema = new schema({
     name: String,
     username: String,
-    profilePic: String,
-    coverPic: String,
+    profile_pic: String,
+    cover_pic: String,
     website: String,
     location: String,
     description: String,
