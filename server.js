e1a104c27c51d7fd519c696ac4a010fbcc13e523
dhanush shetty <dhanush@piktorlabs.com>
2019-11-20 11:16:00 +0530

    testing users api

--- server.js
+++ server.js
@@ -391,20 +391,12 @@ router.route('/user/:userId')
         var query = {
             _id: req.params.userId
         };
-        Users.findOne({
-            username: req.body.username
-        }, function (err, user) {
-            if (user !== null) {
-                res.json({ message: "username not available" })
-            } else {
-                Users.update(query, { $set: req.body }, function (err) {
-                    if (err) {
-                        res.send(err)
-                    }
-                    res.json({ success: "users list updated" })
-                });
+        Users.update(query, { $set: req.body }, function (err) {
+            if (err) {
+                res.send(err)
             }
-        })
+            res.json({ success: "users list updated" })
+        });
     })
 
     .get(function (req, res) {
