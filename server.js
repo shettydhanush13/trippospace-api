58d377bfb2c9eecb5accd76f4d3f4951c4d6bf4b
shettydhanush13 <shettydhanush13@gmail.com>
2020-02-07 15:19:24 +0530

    adding completed trips

--- server.js
+++ server.js
@@ -503,11 +503,10 @@ router.route('/reviews')
 //get completed trips data
 router.route('/completedTripsData/:id')
     .get(function (req, res) {
-    CompletedTripsData.find({"organizerId" : req.params.id.toString()}, function (err, data) {
+    CompletedTripsData.findOne({"organizerId" : req.params.id.toString()}, function (err, data) {
         if (err) {
             res.send(err)
         }
-        console.log("id : ", req.params.id,data)
         res.send(data)
     });
 });
