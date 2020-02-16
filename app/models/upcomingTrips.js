9890a0b20d53a2aff6f67488db76e1f3f60a3510
shettydhanush13 <shettydhanush13@gmail.com>
2020-02-16 21:49:05 +0530

    check exixsting email

---
+++ app/models/upcomingTrips.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose")
+var schema = mongoose.Schema
+
+var upcomingTripsSchema = new schema({
+    thumb:String,
+    tripId:String,
+    tripTitle:String,
+    travelers:Number,
+    credits:Number,
+    date:String,
+    days:Number,
+    userId:String
+});
+
+module.exports = mongoose.model('upcomingTrips', upcomingTripsSchema)
