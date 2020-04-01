c04e3d8bb9d16d6f9796d5eebf2f1406adbf520a
shettydhanush13 <shettydhanush13@gmail.com>
2020-04-01 12:04:07 +0530

    add map blogging functionality

--- server.js
+++ server.js
@@ -1307,7 +1307,7 @@ router.route('/placesSearch')
                     for (let i = 0; i < place.length; i++) {
                         arr2.push(place[i].title)
                     }
-                    Users.find({}, { first_name: 1, last_name:1, _id:1 }, function (err, user) {
+                    Users.find({}, { first_name: 1, last_name:1, _id:1, username:1, profile_pic:1}, function (err, user) {
                         let arr3 = [];
                         for (let i = 0; i < user.length; i++) {
                             arr3.push(user[i])
