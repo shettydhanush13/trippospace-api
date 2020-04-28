1a058fcc450ae821558bc3211fdcc49071a5b6e5
shettydhanush13 <shettydhanush13@gmail.com>
2020-04-28 22:25:39 +0530

    confirmation mail update

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(cors());
+app.use(express.static(path.join(__dirname,'public')))
 
 const accessTokenSecret = 'shettydhanush13@jwt.com';
 
