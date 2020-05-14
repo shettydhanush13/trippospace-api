31e1386a90b1b1c386a40b764f94bc676ac2cbeb
shettydhanush13 <dhanush@piktorlabs.com>
2020-05-14 23:11:04 +0530

    git card code update

--- api/giftCard.js
+++ api/giftCard.js
@@ -12,17 +12,22 @@ router.route('/')
         gift.save(err => err ? res.send(err) : res.json({ message: "gift card created succesfully" }))
     });
 
-router.route('/:code')
-    //to redeem a gift card
+router.route('check/:code')
+    //to check a gift card is valid
     .get((req, res) => {
         const query = { code : req.params.code }
         Gift.findOne(query, (err, card) => err ? res.send(err) 
         : 
         card === null ? res.json({error:"INVALID CODE"})
         :
-        card.redeemed ? res.json({error:"CODE ALREADY REDEEMED"}) 
-        : 
-        Gift.updateOne(query, { $set : { redeemed : true } }, err => err ? res.send(err) : res.json({value: card.value})))
+        card.redeemed ? res.json({error:"CODE ALREADY REDEEMED"}) : res.json({value: ParseInt(card.value)}))
+    });
+
+router.route('apply/:code')
+    //to redeem a gift card
+    .get((req, res) => {
+        const query = { code : req.params.code }
+        Gift.updateOne(query, { $set : { redeemed : true } }, err => err ? res.send(err) : res.json({value: card.value}))
     });
 
 module.exports = router
