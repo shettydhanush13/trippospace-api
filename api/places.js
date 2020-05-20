d5a25c17067f34dcc0d78ddc3db95e8f7403ae3f
shettydhanush13 <dhanush@piktorlabs.com>
2020-05-20 19:01:36 +0530

    adding limit

--- api/places.js
+++ api/places.js
@@ -28,7 +28,7 @@ router.route('/explore/:place')
     //to get place details using place name
     .get((req, res) => {
         const query = { title: req.params.place }
-        Places.find(query, { $limit : 1 }, (err, place) => err ? res.send(err) : res.send(place))
+        Places.find(query, (err, place) => err ? res.send(err) : res.send(place)).limit(1)
     });
     
 router.route('/mutiplePlaces')
