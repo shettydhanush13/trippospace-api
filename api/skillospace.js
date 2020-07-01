97c0ef10f208a86a325fe2912df402114b135d8c
dhanush s shetty <dhanush@piktorlabs.com>
2020-07-01 18:43:48 +0530

    otp for skillospace

--- api/skillospace.js
+++ api/skillospace.js
@@ -8,6 +8,16 @@ const authToken = '755518f7a6b6a99131fd4bb1c5d9d940';
 const servicesId = 'VA744845fc17abd7ed3d9e54547cf76edf'
 const client = require('twilio')(accountSid, authToken);
 
+router.route('/add-user')
+    //to register a new user
+    .post((req, res) => {
+        let skillUser = new SkillUser();
+        skillUser.name = req.body.name;
+        skillUser.phone = req.body.phone;
+        skillUser.email = req.body.email;
+        SkillUser.save((err, user) => err ? res.send(err) : res.json({ success: "user added succesfully", user : user }))
+    });
+
 router.route('/send-otp')
     .post(function (req, res) {
         client.verify.services(servicesId)
@@ -24,6 +34,19 @@ router.route('/verify-otp')
         .then(verification_check => res.send(verification_check.status));
     })
 
+
+    {
+        "info" : {
+            "name" : name.toLowerCase(),
+            "phone" : phone,
+            "email" : email.toLowerCase(),
+            "profile_pic" : Config.profile_pic
+        },
+        "courses" : {
+            "ongoing" : [],
+            "completed" : []
+        }
+    }
 // router.route('/login')
 //     //user login
 //     .post((req, res) => {
