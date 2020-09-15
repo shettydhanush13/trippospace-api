859f05ff8c774d6193e0537e921251eca56b4ef4
dhanush <dhanush@piktorlabs.com>
2020-09-15 22:06:36 +0530

    db change

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 app.use(cors());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_4bnf62cl:659mqm9veus9q1rurnobmbkq93@ds229088.mlab.com:29088/heroku_4bnf62cl');
+// mongoose.connect('mongodb://heroku_4bnf62cl:659mqm9veus9q1rurnobmbkq93@ds229088.mlab.com:29088/heroku_4bnf62cl');
+mongoose.connect('mongodb+srv://dhanush:5$Recieved@cluster0.ejkuv.mongodb.net/trippospace?retryWrites=true&w=majority');
 
 const path = require('path')
 app.use(express.static(path.join(__dirname,'public')))
